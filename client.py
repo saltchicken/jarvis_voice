297e95bc50129c69502689dea87772202f9920e3
saltchicken <john.eicher89@gmail.com>
2024-04-10 21:37:38 -0700

    Initial commit

---
+++ client.py
@@ -0,0 +1,93 @@
+import socket
+import wave
+from loguru import logger
+import pyaudio
+import numpy as np
+import time
+import threading
+
+
+PORT_INC = 10098
+# FORMAT = pyaudio.paFloat32
+
+# Create an instance of ProcessData() to send to server.
+# variable = SocketMessage()
+# # Pickle the object and send it to the server
+# data_string = pickle.dumps(variable)
+# s.send(b'test')
+
+class ChunkSenderThread():
+    def __init__(self):
+        # super().__init__()
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect(('192.168.1.100', PORT_INC+101))
+        
+    def run(self):
+        while True:
+            user_input = input('Enter your speech')
+            
+            if user_input.lower() == 'quit':
+                break
+            
+            self.s.sendall(user_input.encode())
+        logger.debug('Exiting ChunkSender')
+
+
+class SpeakingThread(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        CHANNELS = 1
+        RATE = 12000
+        # CHUNK_SIZE = 1024
+
+        HOST = '192.168.1.100'
+        # Create a socket connection.
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((HOST, PORT_INC + 1))
+        
+
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(format=2,
+                        channels=CHANNELS,
+                        rate=RATE,
+                        output=True)
+
+        # output_wavfile = 'test.wav'
+        # wf = wave.open(output_wavfile, 'wb')
+        # wf.setnchannels(1)
+        # wf.setsampwidth(2)
+        # wf.setframerate(24000)
+        
+    def run(self):
+
+        # audio_collection = b''
+        while True:
+            try:
+                data = self.s.recv(512)
+                logger.debug(f"Received: {len(data)}")
+                self.stream.write(data)
+                # audio_collection += data
+                # wf.writeframes(data)
+            except KeyboardInterrupt as e:
+                logger.debug("Keyboard Interrupt")
+                # stream.write(audio_collection)
+                # time.sleep(2)
+                # wf.close()
+                self.stream.stop_stream()
+                self.stream.close()
+                self.p.terminate()
+                break
+            except Exception as e:
+                print(e)
+
+        self.s.close()
+        
+        
+if __name__ == "__main__":
+    speaking_thread = SpeakingThread()
+    chunk_sender_thread = ChunkSenderThread()
+    speaking_thread.start()
+    chunk_sender_thread.run()
+    
+
+
---
+++ server.py
@@ -0,0 +1,96 @@
+from RealtimeTTS import TextToAudioStream, CoquiEngine
+from loguru import logger
+import socket, time
+
+import threading, queue
+
+from IPython import embed
+
+PORT_INC = 10098
+
+class ChunkReceiverThread(threading.Thread):
+    def __init__(self, queue):
+        super().__init__()
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.queue = queue
+        
+    def run(self):
+        self.socket.bind(('0.0.0.0', PORT_INC+101))
+        self.socket.listen()
+        logger.debug("ChunkReceiver listening")
+        receive_thread = threading.Thread(target=self.handle_client)
+        receive_thread.start()
+            
+    def handle_client(self):
+        conn, addr = self.socket.accept()
+        logger.debug("ChunkReceiver connected")
+        while True:
+            data = conn.recv(1024)
+            logger.debug(f"Received: {data}")
+            self.queue.put(data.decode())
+         
+            
+
+class VoiceGenerator():
+    def __init__(self):
+        
+        self.queue = queue.Queue()
+        
+        chunk_receiver = ChunkReceiverThread(self.queue)
+        chunk_receiver.run()
+        
+        self.engine = CoquiEngine()
+        format, a, b = self.engine.get_stream_info()
+        
+        self.stream = TextToAudioStream(self.engine)
+        
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind(('0.0.0.0', PORT_INC+1))
+        self.server_socket.listen(1)
+        logger.debug('Waiting for connection')
+        self.conn, addr = self.server_socket.accept()
+        logger.debug("VoiceGenerator connected")
+        
+    def on_audio_chunk_callback(self, chunk):
+        logger.debug(f"Chunk received:, len: {len(chunk)}")
+        self.conn.sendall(chunk)
+        
+    def run(self):
+        # TODO: Why is this initialization needed?
+        self.stream.feed('')
+        self.stream.play_async(tokenizer="stanza", language="en", on_audio_chunk=self.on_audio_chunk_callback, muted=True)
+        # self.stream.feed("Test one")
+        while True:
+            text = self.queue.get()
+            logger.debug(f"Text for TTS: {text}")
+            self.stream.feed(text)
+            self.stream.play_async(tokenizer="stanza", language="en", on_audio_chunk=self.on_audio_chunk_callback, muted=True)
+        # embed()
+        # time.sleep(3)
+        # self.stream.feed('Test two')
+        # self.stream.play(tokenizer="stanza", language="en", on_audio_chunk=self.on_audio_chunk_callback, muted=True)
+        
+
+if __name__ == "__main__":
+    voice = VoiceGenerator()
+    voice.run()
+
+    
+    
+# from RealtimeTTS import TextToAudioStream, CoquiEngine
+
+# def dummy_generator():
+#     yield 'Hello there'
+
+# def on_audio_chunk_callback(chunk):
+#     print(f"Chunk received:, len: {len(chunk)}")
+
+# if __name__ == "__main__":
+#     engine = CoquiEngine()
+#     stream = TextToAudioStream(engine)
+#     print("streaming")
+#     stream.feed("Hello")
+#     stream.play_async(tokenizer="stanza", language="en", on_audio_chunk=on_audio_chunk_callback, muted=True)
+
+    #stream.play_async()
+
